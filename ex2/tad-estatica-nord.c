a6e7f111311bec69712206b64299d4938f7a3ff8
vilarinux <davi.r.vilarinho@gmail.com>
2021-08-31 15:19:21 -0300

    shiftando pra remover

--- ex2/tad-estatica-nord.c
+++ ex2/tad-estatica-nord.c
@@ -3,10 +3,14 @@
 #define STDLIB_INCLUIDA 1
 #include <stdlib.h>
 #endif
+#ifndef STRING_INCLUIDA
+#define STRING_INCLUIDA 1
+#include <string.h>
+#endif
 
 struct lista_estatica {
     int tamanho;
-    INFO_ARMAZENADA vetor[MAX_ARMAZENADO][MAX];
+    INFO_ARMAZENADA vetor[MAX][MAX_ARMAZENADO];
 };
 
 lista_p cria_lista() {
@@ -52,11 +56,22 @@ int tamanho(lista_p lista) {
 
 int insere_elem(lista_p lista, INFO_ARMAZENADA_ARGUMENTO inserir) {
     if (!lista_cheia(lista)) {
-        lista->vetor[][tamanho(lista)] = inserir;
+        strcpy(lista->vetor[tamanho(lista)], inserir);
         (lista->tamanho)++;
         return 1;
     } else {
         return 0;
+    }
+}
+
+// encapsulada porque pode quebrar a estrutura
+void shift_esquerda(lista_p lista, int pos) {
+    int tam = tamanho(lista);
+    if (pos > 0 && pos <= tam) {
+        for (int i = pos-1; i < MAX-1; i++) {
+            strcpy(lista->vetor[i], lista->vetor[i+1]);
+        }
+        (lista->tamanho)--;
     }
 }
 
