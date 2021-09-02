d5ddf74bc3d69e4c3cbeab7c70865b8ea24afe35
Appa <72236662+AppaTico@users.noreply.github.com>
2021-09-02 18:02:38 -0300

    Create AED-EX4.h

---
+++ ex4/AED-EX4.h
@@ -0,0 +1,17 @@
+struct no 
+{
+    int info;
+    struct no * prox;
+};
+
+typedef struct no * Lista;
+
+Lista cria_lista();
+int tamanho (Lista *);
+int apaga_lista (Lista *);
+int esvazia_lista (Lista *);
+int insere_ord(Lista *, int);
+int remove_ord (Lista *, int);
+int get_elem_pos (Lista *, int, int *);
+int rem_elem_pos (Lista *, int);
+int intercalar (Lista *, Lista *, Lista *);
